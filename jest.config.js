25ea349baae2beb3fe1aa4d653f7675d9adacf78
xiaolidan <xiaolidan@126.com>
2024-09-20 11:42:39 +0800

    chore: 修复安装包相关问题

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-export default{
-    //tests文件夹下
-    roots: ['<rootDir>/src/tests'],
-    testRegex: '(.+)\\.test\\.(jsx?|tsx?)$',
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest'
-    },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-  };
